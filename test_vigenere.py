3198cd7c848ca11247db10dc27e6b6245cef1766
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-04-27 19:34:40 +0100

    Encrypt working.

--- test_vigenere.py
+++ test_vigenere.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-from unittest import TestCase, main
-from vigenere import generate_table
+from unittest import TestCase,main
+from vigenere import generate_table,encrypt
 
 class TestGenerateTable(TestCase):
   def test_small_alphabet(self):
@@ -9,6 +9,16 @@ class TestGenerateTable(TestCase):
                       ['C','D','A','B'],
                       ['D','A','B','C']],
                      generate_table("ABCD"))
+
+
+class TestEncryptDecrypt(TestCase):
+  def setUp(self):
+    self.table=generate_table("abcdefghijklmnopqrstuvwxyz")
+
+  def test_encrypt(self):
+    self.assertEqual('poesadr',
+                     encrypt(self.table,'donut','maryhad'))
+
 
 if __name__=='__main__':
   main()
--- vigenere.py
+++ vigenere.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from itertools import cycle,izip
 
 def generate_table(alphabet):
   characters=[c for c in alphabet]
@@ -6,3 +7,14 @@ def generate_table(alphabet):
   for i in range(len(characters)):
     table.append(characters[i:]+characters[0:i])
   return table
+
+def encrypt(table,key,plain_text):
+  c_map={}
+  i=0
+  for c in table[0]:
+    c_map[c]=i
+    i+=1
+  cypher_text=[]
+  for pc,k in izip(plain_text,cycle(key)):
+    cypher_text.append(table[c_map[k]][c_map[pc]])
+  return "".join(cypher_text)
